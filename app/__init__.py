c54db505abb18a02f5c4e60868c22d6b03457520
MiguelMunoz555 <m.munoz12@uniandes.edu.co>
2025-03-18 20:00:45 -0500

    Se sube todo el taller 2 modulo 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from app.config.db import db
+from app.config.routes import register_routes
 
-def create_app(config):
+def create_app():
     app = Flask(__name__, template_folder="views")
     # app.config.from_object(config)
 
     db.init_app(app)
+
+    register_routes(app)
 
     # with app.app_context():
     #     db.create_all()
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from app.config.routes import app
+from app import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(debug=True)
