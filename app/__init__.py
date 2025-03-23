f3bea23772d753e64ac7944381cb0598fce3efd8
MiguelMunoz555 <m.munoz12@uniandes.edu.co>
2025-03-23 16:54:34 -0500

    Actualizar taller

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
+from app.config.config import Config
 from app.config.db import db
 from app.config.routes import register_routes
 
 def create_app():
     app = Flask(__name__, template_folder="views")
-    # app.config.from_object(config)
+
+    app.config.from_object(Config)
+
+    # 🔥 Agrega este print para ver si la variable se carga
+    print("DATABASE_URI en Render:", app.config.get("SQLALCHEMY_DATABASE_URI"))
+
+    if not app.config.get("SQLALCHEMY_DATABASE_URI"):
+        raise RuntimeError("SQLALCHEMY_DATABASE_URI no está definida.")
 
     db.init_app(app)
-
     register_routes(app)
-
-    # with app.app_context():
-    #     db.create_all()
 
     return app
--- app/config/config.py
+++ app/config/config.py
@@ -1,8 +1,5 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv(override=True)
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///fallback.db")  # Usa SQLite si la variable está vacía
     SQLALCHEMY_TRACK_MODIFICATIONS = False
